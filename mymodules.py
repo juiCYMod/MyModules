2e54aab0e31755534f10196ca8ed275faf712d1a
juiCYMod <34161692+juiCYMod@users.noreply.github.com>
2019-01-10 22:30:27 +0900

    Add files via upload

---
+++ mymodules.py
@@ -0,0 +1,25 @@
+import sys
+import time
+import requests
+import configparser
+
+inifile = configparser.ConfigParser()
+inifile.read('./config.ini', 'UTF-8')
+
+def stringer(string):
+    mojisu = len(string)
+    for i in range(mojisu):
+        sys.stdout.write(string[i])
+        sys.stdout.flush()
+        time.sleep(0.025)
+
+def line(message):
+    url = "https://notify-api.line.me/api/notify"
+    token = inifile.get('settings', 'token')
+    headers = {"Authorization" : "Bearer "+ token}
+    payload = {"message" :  message}
+    try:
+        r = requests.post(url ,headers = headers ,params=payload)
+    except Exception as e:
+        print(e) 
+
